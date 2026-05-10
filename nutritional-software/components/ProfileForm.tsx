842fa990c3a99b736122e33f77cc7e701ed69dbd
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2026-05-10 19:24:08 +1200

    Update ProfileForm.tsx

--- nutritional-software/components/ProfileForm.tsx
+++ nutritional-software/components/ProfileForm.tsx
@@ -264,7 +264,13 @@ export default function ProfileForm() {
                 </strong>
               </div>
 
-
+              <div>
+                <span>Medical conditions</span>
+                <strong>
+                  {submittedProfile.medicalConditions || "Not provided"}
+                </strong>
+              </div>
+            </div>
           )}
         </aside>
       </div>
