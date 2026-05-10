f74ba71a69e1df9c85df1340f47cdce059407699
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2026-05-10 18:48:50 +1200

    s

--- nutritional-software/app/profile/page.tsx
+++ nutritional-software/app/profile/page.tsx
@@ -1,12 +1,12 @@
-// Author: Marty
+// Author: Marty Orchard
 // Area: Frontend / UI
 
 import ProfileForm from "@/components/ProfileForm";
 
 export default function ProfilePage() {
-    return (
-        <main className="page-shell">
-            <ProfileForm />
-        </main>
-    );
+  return (
+    <main className="page-shell">
+      <ProfileForm />
+    </main>
+  );
 }
--- nutritional-software/components/ProfileForm.tsx
+++ nutritional-software/components/ProfileForm.tsx
@@ -1,19 +1,6 @@
-// Author: Marty
+// Author: Marty Orchard
 // Area: Frontend / UI
 
 "use client";
 
 import { useState } from "react";
-
-type ProfileFormData = 
-{
-    patientName: string;
-    age: string;
-    gender: string;
-    ethnicity: string;
-    height: string;
-    weight: string;
-    activityLevel: string;
-    dietaryRestrictions: string;
-    medicalConditions: string;
-}
