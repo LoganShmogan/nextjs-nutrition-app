9e37a3305a9c5d552be5c35209697e5751493844
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2026-05-10 20:42:45 +1200

    Call component

---
+++ nutritional-software/app/analysis/page.tsx
@@ -0,0 +1,13 @@
+// Author: Marty Orchard
+// Area: Frontend / UI
+
+import Analysis from "@/components/analysis";
+import styles from "./page.module.css";
+
+export default function AnalysisPage() {
+  return (
+    <main className={styles.pageShell}>
+      <Analysis />
+    </main>
+  );
+}
