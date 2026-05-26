ea6bc43f069b55d047f27aa8a973d74f7bbed878
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2026-05-26 20:08:14 +1200

    Update foods.ts

--- nutritional-software/lib/mock-data/foods.ts
+++ nutritional-software/lib/mock-data/foods.ts
@@ -1,32 +1,3 @@
 // Author: Logan 
 // Area: Backend / API / Logic / Testing
 
-export type NutrientValue = number | null;
-
-export type FoodNutrients = {
-  energyKcal: NutrientValue;
-  energyKj: NutrientValue;
-  protein: NutrientValue;
-  carbohydrate: NutrientValue;
-  fat: NutrientValue;
-  sugar: NutrientValue;
-  sodium: NutrientValue;
-  fibre: NutrientValue;
-  calcium: NutrientValue;
-  iron: NutrientValue;
-  vitaminC: NutrientValue;
-};
-
-export type Food = {
-  id: string;
-  name: string;
-  shortName: string;
-  description: string;
-  category: string;
-  nutrients: FoodNutrients;
-};
-
-export type FoodSearchResult = Pick<
-  Food,
-  "id" | "name" | "shortName" | "description" | "category" | "nutrients"
->;
